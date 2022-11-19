d7fa50d6a1d7fec827571b65567014f41debf9e1
bec-barnett <rebeccalucybarnett@gmail.com>
2022-11-19 14:10:50 +1100

    added home seciton

--- js/script.js
+++ js/script.js
@@ -40,3 +40,28 @@ function displayDescription(selectedDrink) {
     x.style.display = "block";
 
   }
+
+// Image Slideshow. Source: https://www.w3schools.com/howto/howto_js_slideshow.asp
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("our-story-slideshow");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+} 
